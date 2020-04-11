4f1132bb955cb95cd5107b1db7a03b4b1ee3b2e3
Scott Nichols <snichols@vmware.com>
2020-04-11 09:29:03 -0700

    adding the add event command and documenting some plans.

---
+++ pkg/commands/event.go
@@ -0,0 +1,63 @@
+package commands
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/n3wscott/bujo/pkg/commands/options"
+	"github.com/n3wscott/bujo/pkg/glyph"
+	"github.com/n3wscott/bujo/pkg/runner/add"
+	"github.com/n3wscott/bujo/pkg/store"
+	base "github.com/n3wscott/cli-base/pkg/commands/options"
+	"github.com/spf13/cobra"
+)
+
+func addEvent(topLevel *cobra.Command) {
+	no := &options.AddOptions{}
+	so := &options.SigOptions{}
+	co := &options.CollectionOptions{}
+
+	// TODO: change this to be bujo task add x y z
+	// TODO: make a demo of bujo task list
+
+	cmd := &cobra.Command{
+		Use:   "event",
+		Short: "Add an event",
+		Example: `
+bujo add event a fun party
+`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("requires an event title")
+			}
+			no.Message = strings.Join(args, " ")
+
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			p, err := store.Load(nil)
+			if err != nil {
+				return err
+			}
+			s := add.Add{
+				Bullet:        glyph.Event,
+				Persistence:   p,
+				Message:       no.Message,
+				Collection:    co.Collection,
+				Priority:      so.Priority,
+				Inspiration:   so.Inspiration,
+				Investigation: so.Investigation,
+			}
+			err = s.Do(context.Background())
+			return oo.HandleError(err)
+		},
+	}
+
+	options.AddNoteArgs(cmd, no)
+	options.AddSigArgs(cmd, so)
+	options.AddCollectionArgs(cmd, co)
+
+	base.AddOutputArg(cmd, oo)
+	topLevel.AddCommand(cmd)
+}
